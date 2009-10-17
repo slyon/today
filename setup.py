c87df8d1da36036e2f66eca1f814b00fe515868d
Lukas Märdian <lukasmaerdian@gmail.com>
2009-10-17 12:06:14 +0200

    fix typo

--- setup.py
+++ setup.py
@@ -36,6 +36,6 @@ setup(
     data_files = [
         ( "../../etc", ["data/shr-today.conf"] ),
         ( "shr-today", ["data/wallpaper.png", "data/slider.edj"] ),
-        ( "../../etc/X11/Xsession.d", ["data/89shr-today" ),
+        ( "../../etc/X11/Xsession.d", ["data/89shr-today"] ),
         ]
 )
